aa2178b35603c667bbb8b0b8f165c5a6ad90d1a4
Shubham Gupta <gupta.shubham97@gmail.com>
2024-10-10 14:56:26 +0530

    implement writeSchema()

--- src/main/java/social/bsky/shubhamg/spicedb_tester/App.java
+++ src/main/java/social/bsky/shubhamg/spicedb_tester/App.java
@@ -2,8 +2,10 @@ package social.bsky.shubhamg.spicedb_tester;
 
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.Logger;
 
 public class App {
+	private static final Logger logger = Logger.getLogger(App.class.getName());
 	private static final Properties properties = new Properties();
 	
 	public static void readProperties(String filename) {
@@ -25,10 +27,43 @@ public class App {
     public static void main( String[] args ){
     	readProperties("application.properties");
     	
-    	String accessToken = properties.getProperty("access.token");
-    	String target = properties.getProperty("target");
+    	final String ACCESS_TOKEN = properties.getProperty("access.token");
+    	final String TARGET = properties.getProperty("target");
+    	final String PERMISSION_SYSTEM = properties.getProperty("permission.system");
     	
-    	SpiceDBClient spiceDBClient = new SpiceDBClient(accessToken, target);
+    	SpiceDBClient spiceDBClient = new SpiceDBClient(TARGET, ACCESS_TOKEN);
+    	
+    	String schema = """
+    			definition PERMISSION_SYSTEM/user {}
+    			
+    			definition PERMISSION_SYSTEM/group {
+    				relation member: PERMISSION_SYSTEM/user
+    			}
+    			
+    			definition PERMISSION_SYSTEM/event {
+    				relation recruiter: PERMISSION_SYSTEM/user | PERMISSION_SYSTEM/group#member
+    				relation panelist: PERMISSION_SYSTEM/user | PERMISSION_SYSTEM/group#member
+    				relation coordinator: PERMISSION_SYSTEM/user | PERMISSION_SYSTEM/group#member
+    				relation owner: PERMISSION_SYSTEM/user | PERMISSION_SYSTEM/group#member
+    				
+    				permission view = panelist + manage
+    				permission manage = recruiter + delete
+    				permission delete = owner + coordinator
+    			}
+    			
+    			definition PERMISSION_SYSTEM/job {
+    				relation reader: PERMISSION_SYSTEM/user
+    				relation owner: PERMISSION_SYSTEM/user
+    				
+    				permission view = reader + edit
+    				permission edit = owner
+    				permission delete = owner
+    			}
+    	""";
+    	schema = schema.replaceAll("PERMISSION_SYSTEM", PERMISSION_SYSTEM);
+    	
+    	String writeSchemaResponse = spiceDBClient.writeSchema(schema);
+    	System.out.println(writeSchemaResponse);
     	
     }
 }
--- src/main/java/social/bsky/shubhamg/spicedb_tester/SpiceDBClient.java
+++ src/main/java/social/bsky/shubhamg/spicedb_tester/SpiceDBClient.java
@@ -1,21 +1,64 @@
 package social.bsky.shubhamg.spicedb_tester;
 
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.authzed.api.v1.PermissionsServiceGrpc;
+import com.authzed.api.v1.SchemaServiceGrpc;
 import com.authzed.api.v1.PermissionService.CheckPermissionResponse.Permissionship;
+import com.authzed.api.v1.SchemaServiceOuterClass.WriteSchemaRequest;
+import com.authzed.api.v1.SchemaServiceOuterClass.WriteSchemaResponse;
+import com.authzed.grpcutil.BearerToken;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
 
 public class SpiceDBClient {
 	private static final Logger logger = Logger.getLogger(SpiceDBClient.class.getName());
-    private final String target;
-    private final String token;
-    
-    SpiceDBClient(String target, String token) {
-    	this.target = target;
-    	this.token = token;
-    }
-    
-    public String writeSchema() { return ""; }
-    public String readSchema() { return ""; }
-    public Permissionship checkPermission() { return null; }
-    
+	private final String target;
+	private final String token;
+	
+	private final SchemaServiceGrpc.SchemaServiceBlockingStub schemaService;
+	private final PermissionsServiceGrpc.PermissionsServiceBlockingStub permissionsService;
+
+	SpiceDBClient(String target, String token) {
+		this.target = target;
+		this.token = token;
+		
+		ManagedChannel channel = ManagedChannelBuilder
+				.forTarget(target)
+				.useTransportSecurity()
+				.build();
+		
+		BearerToken bearerToken = new BearerToken(token);
+        schemaService = SchemaServiceGrpc.newBlockingStub(channel)
+                .withCallCredentials(bearerToken);
+        permissionsService = PermissionsServiceGrpc.newBlockingStub(channel)
+                .withCallCredentials(new BearerToken(token));
+	}
+
+	public String writeSchema(String schema) {
+		logger.info("Writing schema...");
+		
+		WriteSchemaRequest request = WriteSchemaRequest
+				.newBuilder()
+				.setSchema(schema)
+				.build();
+		
+		WriteSchemaResponse response;
+		try {
+            response = schemaService.writeSchema(request);
+        } catch (Exception e) {
+            logger.log(Level.WARNING, "RPC failed: {0}", e.getMessage());
+            return "";
+        }
+        logger.info("Response: " + response.toString());
+        
+        return response.toString();
+	}
+
+	public String readSchema() { return ""; }
+
+	public Permissionship checkPermission() { return null; }
+
 }
