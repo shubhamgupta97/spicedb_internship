93fa97331b78a18702e12e949e49debf04b78835
Shubham Gupta <gupta.shubham97@gmail.com>
2024-10-10 13:21:44 +0530

    initial maven scafolding

---
+++ src/main/java/social/bsky/shubhamg/spicedb_tester/App.java
@@ -0,0 +1,13 @@
+package social.bsky.shubhamg.spicedb_tester;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
