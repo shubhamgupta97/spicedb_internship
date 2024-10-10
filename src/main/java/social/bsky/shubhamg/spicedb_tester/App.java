3a16635aaf3e93d3b1ac186708f4c669f5c11fd2
Shubham Gupta <gupta.shubham97@gmail.com>
2024-10-10 13:43:06 +0530

    add configuration loader

--- src/main/java/social/bsky/shubhamg/spicedb_tester/App.java
+++ src/main/java/social/bsky/shubhamg/spicedb_tester/App.java
@@ -1,13 +1,33 @@
 package social.bsky.shubhamg.spicedb_tester;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.InputStream;
+import java.util.Properties;
+
+public class App {
+	private static final Properties properties = new Properties();
+	
+	public static void readProperties(String filename) {
+		String filepath = "config/" + filename;
+    	
+    	try (InputStream input = App.class.getClassLoader().getResourceAsStream(filepath)) {
+    		if(input == null) {
+    			System.out.println("Sorry, unable to find " + filename);
+                return;
+            }
+            
+            properties.load(input);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+	}
+	
+    public static void main( String[] args ){
+    	readProperties("application.properties");
+    	
+    	String accessToken = properties.getProperty("access.token");
+    	
+    	System.out.println(accessToken);
+    	
     }
 }
